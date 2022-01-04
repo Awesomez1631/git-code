bac9855d946ed5952a6653ae5981f4307f7219a1
Awesomez1631 <getchaitanyabhargava@gmail.com>
2022-01-04 16:45:15 +0530

    second commit

---
+++ text.js
@@ -0,0 +1,8 @@
+Red Riding Hood
+there was a little boy and he got some cookies to gift to his sick grandmother. 
+When he reached the house, instead of his grandmother there was a wolf
+Red Riding Hood
+there was a little boy and he got some cookies to gift to his sick grandmother. 
+When he reached the house, instead of his grandmother there was a wolf
+the wolf had big teeth, big toe and a big mouth
+the child got help from the woodcutter and killed the wolf
